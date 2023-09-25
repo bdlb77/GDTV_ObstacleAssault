ee98980da0110eb50e92281ce4810d235143537e
bdlb77 <bryan.leigh77@gmail.com>
2023-09-24 23:54:20 -0700

    add actor c++ script

--- Source/GDTV_ObstacleAssault/MovingPlatform.h
+++ Source/GDTV_ObstacleAssault/MovingPlatform.h
@@ -10,8 +10,8 @@ UCLASS()
 class GDTV_OBSTACLEASSAULT_API AMovingPlatform : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AMovingPlatform();
 
@@ -19,8 +19,13 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditAnywhere)
+	int32 MyInt = 99;
+	
+	UPROPERTY(EditAnywhere)
+	bool IsMoving = false;
 };
